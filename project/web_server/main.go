0253f8bfd1d1e215fcc01aed14a806825bb51f99
chuxiwen <1609185446@qq.com>
2023-10-02 16:57:17 +0800

    创建一个简单的web

---
+++ project/web_server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("project/web_server/static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/hello" {
+		http.Error(writer, "404 NOT FOUND", http.StatusFound)
+		return
+	}
+	if request.Method != "GET" {
+		http.Error(writer, "method is not supported", http.StatusFound)
+		return
+	}
+	fmt.Fprintf(writer, "hello!")
+}
+
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+	if err := request.ParseForm(); err != nil {
+		fmt.Fprintf(writer, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(writer, "POST request successful!!")
+	name := request.FormValue("name")
+	address := request.FormValue("address")
+	fmt.Fprintf(writer, "Name = %s\n", name)
+	fmt.Fprintf(writer, "Address = %s\n", address)
+}
