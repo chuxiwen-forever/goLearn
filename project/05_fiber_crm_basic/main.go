14ac3f387e0ea4f54a2df547d2c3738668b1f6cb
chuxiwen <1609185446@qq.com>
2023-10-06 17:08:02 +0800

    完善项目5业务

--- project/05_fiber_crm_basic/main.go
+++ project/05_fiber_crm_basic/main.go
@@ -4,22 +4,23 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/liu/fiber-crm-basic/database"
 	"github.com/liu/fiber-crm-basic/lead"
 )
 
 func setupRoutes(app *fiber.App) {
-	app.Get("", lead.GetLeads)
-	app.Get("", lead.GetLead)
-	app.Post("", lead.NewLead)
-	app.Delete("", lead.DeleteLead)
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
 }
 
 func initDatabase() {
 	var err error
 	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
 	if err != nil {
-		panic("failed to connect database")
+		panic(err)
 	}
 	fmt.Println("Connection opened to database")
 	database.DBConn.AutoMigrate(&lead.Lead{})
